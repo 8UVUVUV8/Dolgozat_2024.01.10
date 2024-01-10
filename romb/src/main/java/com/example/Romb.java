40eacdde0c6a16d7cac8cb962470c78b5982c8e8
szekeres miklos <hiimmikescott@gmail.com>
2024-01-10 11:15:07 +0100

    Teszt14

--- romb/src/main/java/com/example/Romb.java
+++ romb/src/main/java/com/example/Romb.java
@@ -3,8 +3,10 @@ package com.example;
 public class Romb {
     public Double calcArea(Double side, Double alpha){
         if (side <= 0.0 || alpha <=0.0) {
+            throw new IllegalArgumentException();  
+        }
+        if (alpha>=180) {
             throw new IllegalArgumentException();
-            
         }
         Double sideSqrt = Math.pow(side, 2);
         Double alpSin = Math.sin(Math.toRadians(alpha));
