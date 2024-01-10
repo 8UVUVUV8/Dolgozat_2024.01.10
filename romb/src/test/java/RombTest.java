8c8fca3ac853267c1f0c25aa3003b8521ceac424
szekeres miklos <hiimmikescott@gmail.com>
2024-01-10 10:24:46 +0100

    Teszt2

--- romb/src/test/java/RombTest.java
+++ romb/src/test/java/RombTest.java
@@ -1,5 +1,11 @@
+import org.testng.annotations.BeforeTest;
+
 import com.example.Romb;
 
 public class RombTest {
     Romb romb;
+    @BeforeTest
+    public void setUp(){
+        this.romb = new Romb();
+    }
 }
