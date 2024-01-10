befc2655611dac055df66b4058934966a255d5c5
szekeres miklos <hiimmikescott@gmail.com>
2024-01-10 11:06:30 +0100

    Teszt12

--- romb/src/main/java/com/example/Romb.java
+++ romb/src/main/java/com/example/Romb.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class Romb {
     public Double calcArea(Double side, Double alpha){
+        if (side <= 0.0 || alpha <=0.0) {
+            throw new IllegalArgumentException();
+            
+        }
         Double sideSqrt = Math.pow(side, 2);
         Double alpSin = Math.sin(Math.toRadians(alpha));
         Double result = sideSqrt*alpSin;
--- romb/src/test/java/RombTest.java
+++ romb/src/test/java/RombTest.java
@@ -11,12 +11,23 @@ public class RombTest {
         this.romb = new Romb();
     }
     @Test
+    public void testArgumentType(){
+        String doubleString = "Double";
+        String doubleTest = "";
+        Number result = this.romb.calcArea(10.0,90.0);
+        if (result instanceof Double) {
+            doubleTest = "Double";
+        }
+        Assert.assertEquals(doubleString, doubleTest);
+    }
+
+    @Test
     public void testCalcArea_Side10_Alpha90(){
         Double result = this.romb.calcArea(10.0,90.0);
         Double expected = 100.0;
         Assert.assertEquals(result, expected, 0.1);
     }
-    @Test(expectedExceptions = IllegalArgumentException.class);
+    @Test(expectedExceptions = IllegalArgumentException.class)
     public void testCalcArea_Side0_Alpha0(){
         this.romb.calcArea(0.0, 0.0);
     }
