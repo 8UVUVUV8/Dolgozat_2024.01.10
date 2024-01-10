c6ebe45b6c3debaf5a8e59fef9439e4956f68973
szekeres miklos <hiimmikescott@gmail.com>
2024-01-10 10:37:56 +0100

    Teszt9

--- romb/src/test/java/RombTest.java
+++ romb/src/test/java/RombTest.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -12,5 +13,7 @@ public class RombTest {
     @Test
     public void testCalcArea_Side10_Alpha20(){
         Double result = this.romb.calcArea(10.0,20.0);
+        Double expected = 0.7660;
+        Assert.assertEquals(result, expected, 0.1);
     }
 }
